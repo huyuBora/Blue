28f6aa51d82145f6fc00fecee3a14e7f9f72bdbc
huyuBora <huyubora@gmail.com>
2023-01-03 20:04:12 +0900

    C

--- replica.js
+++ replica.js
@@ -11,6 +11,26 @@ function loadEvent(){
         }
     });/** scroll event end **/
 
+    /** section event **/
+    function section(){
+        let section = document.querySelectorAll(".section");
+
+        let sectionA = new IntersectionObserver((event)=>{
+            event.forEach((e)=>{
+                if(e.isIntersecting){
+                    e.target.classList.add("Map");
+                }else{
+                    e.target.classList.remove("Map");
+                }
+            });
+        });
+
+        sectionA.observe(section[0]);
+        sectionA.observe(section[1]);
+        sectionA.observe(section[2]);
+    }/** section event end **/
+    section();
+
     function ToBtncEvent(){
         let ToBtn = document.getElementById("ToBtn");
         let ToBtnBox = document.getElementById("ToBtn").querySelector(".ToBtnBox");
@@ -55,13 +75,98 @@ function loadEvent(){
                     break;
 
                     default:
-
                     break;
                 }
             });
         }
     }/** ToBtn Event end */
     ToBtncEvent();
+
+    function firstEvent(){
+        let linkPhoto = document.getElementById("first").querySelector(".linkPhoto");
+        let linkPhotoA = document.getElementById("first").querySelectorAll(".linkPhoto > ul > a");
+        for(let i = 0; i < linkPhotoA.length; i++){
+            /** window load event **/
+            window.addEventListener("load", function(){
+                if(window.innerWidth >= 768){
+                    setTimeout(() => {
+                        linkPhotoA[i].style.left = "0px";
+                        linkPhotoA[i].style.opacity = "0.4";
+                        linkPhotoA[i].style.visibility = "visible";
+                    }, i * 300);
+                }else{
+                    linkPhotoA[i].style.left = "-300px";
+                    linkPhotoA[i].style.opacity = "0";
+                    linkPhotoA[i].style.visibility = "hieddn";
+                }
+            });
+            /** window resize event **/
+            window.addEventListener("resize", function(){
+                if(window.innerWidth >= 768){
+                    setTimeout(() => {
+                        linkPhotoA[i].style.left = "0px";
+                        linkPhotoA[i].style.opacity = "0.4";
+                        linkPhotoA[i].style.visibility = "visible";
+                    }, i * 300);
+                }else{
+                    setTimeout(() => {
+                        linkPhotoA[i].style.left = "-300px";
+                        linkPhotoA[i].style.opacity = "0";
+                        linkPhotoA[i].style.visibility = "hieddn";
+                    }, i * 300);
+                }
+            });
+            /** window mouseover event **/
+            linkPhotoA[i].addEventListener("mouseover", function(){
+                linkPhotoA[i].style.opacity = 0.8;
+            });
+            /** window mouseout event **/
+            linkPhotoA[i].addEventListener("mouseout", function(){
+                linkPhotoA[i].style.opacity = 0.4;
+            });
+        }
+
+        let firstListA = document.getElementById("first").querySelectorAll(".firstList > ul > a");
+        for(let i = 0; i < firstListA.length; i++){
+            firstListA[i].addEventListener("click", function(){
+                switch (firstListA[i]) {
+                    case firstListA[0]:
+                        document.getElementById("second").scrollIntoView({
+                            behavior : "smooth",
+                            inline : "start",
+                            block : "start"
+                        });
+                        break;
+                
+                    default:
+                        break;
+                }
+            });
+        }
+    }/** first event end */
+    firstEvent();
+
+    function secondEvent(){
+        let intA = document.getElementById("second").querySelectorAll(".intA");
+        let intB = document.getElementById("second").querySelectorAll(".intB");
+        let Soop = document.querySelector(".Soop");
+
+        for(let i = 0; i < intB.length; i++){
+            intB[i].addEventListener("click", function(){
+                for(let j = 0; j < intA.length; j++){
+                    intA[i].classList.add("Soop");
+                }
+            });
+        }
+        Soop.addEventListener("click", function(){
+            Soop.classList.remove(".Soop");
+        });
+    }/** second event end */
+    secondEvent();
+
+
 }/** load event end **/
 loadEvent();
 
+let intA = document.getElementById("second").querySelectorAll(".intA");
+let intB = document.getElementById("second").querySelectorAll(".intB");
