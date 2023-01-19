8e96ab1698014dfb83f53beaea65a57f92cda1af
huyuBora <huyubora@gmail.com>
2023-01-19 23:57:31 +0900

    A

--- replica.js
+++ replica.js
@@ -10,19 +10,81 @@ function blueEvent(){
     });
 
     let btnLogo = document.getElementById("first").querySelector(".btnLogo");
-    let btnLink = document.getElementById("first").querySelectorAll(".btnLink");
-    for(let i = 0; i < btnLink.length; i++){
+    let btnLink = document.getElementById("first").querySelectorAll(".btnLogo > ul > li");
+    for (let i = 0; i < btnLink.length; i++) {
         window.addEventListener("load", ()=>{
             if(window.innerWidth >= 1024){
-                setTimeout(() => {
-                    btnLink[i].style.left = "0px";
-                }, i * 300);
+                btnLink[i].style.left = "5px";
             }else{
-                btnLink[i].style.left = "-250px";
+                btnLink[i].style.left = "-180px";
+            }
+        });/** load **/
+        let timer = null;
+        let sec = 1000;
+        window.addEventListener("resize", ()=>{
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                if(window.innerWidth >= 1024){
+                    setTimeout(() => {
+                        btnLink[i].style.left = "5px";
+                    }, i * 300);
+                    btnLink[i].addEventListener("mouseover", ()=>{
+                        btnLink[i].style.left = "20px";
+                    });
+                    btnLink[i].addEventListener("mouseout", ()=>{
+                        btnLink[i].style.left = "5px";
+                    });
+                }else if(window.innerWidth >= 768 && window.innerWidth < 1024){
+                    setTimeout(() => {
+                        btnLink[i].style.left = "-180px";
+                    }, i * 300);
+                    btnLink[i].addEventListener("mouseover", ()=>{
+                        btnLink[i].style.left = "15px";
+                    });
+                    btnLink[i].addEventListener("mouseout", ()=>{
+                        btnLink[i].style.left = "-180px";
+                    });
+                }else{
+                    btnLink[i].style.left = "-300px";
+                }
+            }, sec);
+        });/** resize **/
+        btnLink[i].addEventListener("mouseover", ()=>{
+            if(window.innerWidth >= 1024){
+                btnLink[i].style.left = "20px";
+            }else if(window.innerWidth >= 768 && window.innerWidth < 1024){
+                btnLink[i].style.left = "15px";
             }
         });
-        window.addEventListener("resize", ()=>{
-            
+        btnLink[i].addEventListener("mouseout", ()=>{
+            if(window.innerWidth >= 1024){
+                btnLink[i].style.left = "5px";
+            }else if(window.innerWidth >= 768 && window.innerWidth < 1024){
+                btnLink[i].style.left = "-180px";
+            }
+        });
+
+        btnLink[i].addEventListener("dblclick", ()=>{
+            switch(btnLink[i]) {
+                case btnLink[0]:
+                    window.open('https://www.sonymusic.co.jp/artist/ReoNa/info/545241?_ga=2.240207875.1421339600.1674093715-80469984.1670901717');
+                    break;
+                case btnLink[1]:
+                    window.open('https://www.sonymusic.co.jp/artist/ReoNa/info/545240?_ga=2.251177222.1421339600.1674093715-80469984.1670901717');
+                    break;
+                case btnLink[2]:
+                    window.open('https://reonafc.com/s/n78/?ima=0320');
+                    break;
+                case btnLink[3]:
+                    window.open('https://www.sonymusicshop.jp/m/sear/groupShw.php?site=S&ima=0335&cd=G001074');
+                    break;
+                case btnLink[4]:
+                    window.open('https://www.sonymusic.co.jp/artist/ReoNa/discography/VVCL-2072');
+                    break;
+                default:
+                    undefined;
+                    break;
+            }
         });
     }
 
@@ -41,8 +103,26 @@ function blueEvent(){
     sectionBox.observe(section[1]);/** second **/
     sectionBox.observe(section[2]);/** third **/
     sectionBox.observe(section[3]);/** fourth **/
+
+    /****/
+    let first = document.getElementById("first");
+
+
+    
+    let fiMenu = document.getElementById("first").querySelector(".fiMenu");
+    let firstListBox = document.getElementById("first").querySelector(".list");
+    let firstList = document.getElementById("first").querySelectorAll(".list > ul > li");
+    let firstListView = new IntersectionObserver((firstFor)=>{
+        firstFor.forEach((firstListA)=>{
+            if(!firstListA.isIntersecting){
+                fiMenu.classList.add("neb");
+            }else{
+                fiMenu.classList.remove("neb");
+            }
+        })
+    })
+
+    firstListView.observe(first);
 }
 blueEvent();
-
-let btnLink = document.getElementById("first").querySelectorAll(".btnLink");
-let btnLogo = document.getElementById("first").querySelector(".btnLogo");
+let firstList = document.getElementById("first").querySelectorAll(".list > ul > li");
