8fe3091565b62747d70720ea7210cce423db7d46
huyuBora <huyubora@gmail.com>
2023-02-11 21:48:51 +0900

    A

--- replica.js
+++ replica.js
@@ -9,12 +9,36 @@ function BlueEvent(){
     let section = document.querySelectorAll(".section");
     let sectionIndex = section.length;
 
+    let box = document.querySelectorAll(".box");
+
     let first = document.getElementById("first");
 
     let display = document.getElementById("display");
     let displayAll = document.getElementById("display").querySelectorAll("ul > li");
 
     let timer = null;
+
+    /** window resize **/
+    window.addEventListener("resize", () => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            /** window 768 down **/
+            if(window.innerWidth < 768){
+                display.style.left = "-300px";
+            }else{
+                display.style.left = "5px";
+            }
+
+            /** box evnet **/
+            for(let i = 0; i < box.length; i++){
+                if(window.innerWidth < 768){
+                    box[i].style.width = "100%";
+                }else{
+                    box[i].style.width = "calc(100% - 100px)";
+                }
+            }
+        }, 300);
+    });
     /** window wheel **/
     window.addEventListener("mousewheel", (e)=>{
         clearTimeout(timer);
@@ -30,6 +54,8 @@ function BlueEvent(){
                     downClick.style.visibility = "visible";
                     /****/
                     display.classList.add("app");
+
+                    
                 }
                 if(pageNumber == sectionIndex){
                     upClick.style.opacity = 1;
@@ -63,6 +89,7 @@ function BlueEvent(){
             }
         }, 300);
     });
+    /** page up **/
     function pageUp(){
         if(pageNumber > 1){
             upClick.removeAttribute("disabled"); // 속성 제거
@@ -86,6 +113,8 @@ function BlueEvent(){
             display.classList.remove("app");
         }
     }
+
+    /** page Down **/
     function pageDown(){
         if(pageNumber < sectionIndex){
             position += window.innerHeight; // height size 증가
@@ -107,18 +136,50 @@ function BlueEvent(){
         }
     }
 
+    /** display Event **/
     function displayEvent(){
         for (let i = 0; i < displayAll.length; i++) {
             displayAll[i].addEventListener("click", ()=> {
                 switch(displayAll[i]) {
                     case displayAll[0]:
                         if(pageNumber != 1){
-                            console.log("ss");
+                            position = 0;
+                            container.style.transform = `translateY(${-position}px)`
                             pageNumber = 1;
+
+                            upClick.style.opacity = 0;
+                            upClick.style.visibility = "hidden";
+                            display.classList.remove("app");
                         }
-                        console.log(pageNumber);
                         break;
-                
+                    case displayAll[1]: // -832
+                        if(pageNumber != 2){
+                            position = window.innerHeight;
+                            container.style.transform = `translateY(${-position}px)`
+                            pageNumber = 2;
+                        }
+                        break;
+                    case displayAll[2]: // 832 * 2 가 되어야 하는데...
+                        if(pageNumber != 3){
+                            position = window.innerHeight*2 ;
+                            container.style.transform = `translateY(${-position}px)`
+                            pageNumber = 3;
+                        }
+                        break;
+                    case displayAll[3]: // 832 * 2 가 되어야 하는데...
+                        if(pageNumber != 4){
+                            position = window.innerHeight*3 ;
+                            container.style.transform = `translateY(${-position}px)`
+                            pageNumber = 4;
+                        }
+                        break;
+                    case displayAll[4]: // 832 * 2 가 되어야 하는데...
+                        if(pageNumber != 5){
+                            position = window.innerHeight*4 ;
+                            container.style.transform = `translateY(${-position}px)`
+                            pageNumber = 5;
+                        }
+                        break;
                     default:
                         break;
                 }
@@ -126,8 +187,6 @@ function BlueEvent(){
         }
     }
     displayEvent();
-
-
 
     function pageInit(){
         upClick.removeAttribute("disabled"); // 속성 제거
@@ -214,7 +273,7 @@ function BlueEvent(){
     function sectionEvent(){
         let options = {
             root: document.querySelector('html'), // .container class를 가진 엘리먼트를 root로 설정. null일 경우 브라우저 viewport
-            rootMargin: '-100px', // rootMargin을 '10px 10px 10px 10px'로 설정
+            rootMargin: '-50px', // rootMargin을 '10px 10px 10px 10px'로 설정
             threshold: 0.3
         }
 
@@ -267,5 +326,23 @@ function BlueEvent(){
         sectionObserver.observe(section[4]);
     }
     sectionEvent();
+
+    let firstMenu = document.getElementById("first").querySelector(".firstMenu");
+    let firstMenuBtn = document.getElementById("first").querySelector(".firstMenuBtn");
+    function firstMEnuEvent(){
+        firstMenuBtn.addEventListener("click", ()=> {
+            firstMenu.classList.toggle("expans");
+
+            if(firstMenu.classList.contains("expans")){
+                firstMenuBtn.classList.add("openFiMenu")
+            }else if(!firstMenu.classList.contains("expans")){
+                firstMenuBtn.classList.remove("openFiMenu")
+            }
+
+        });
+    }
+    firstMEnuEvent();
+    
+
 }
 BlueEvent();
